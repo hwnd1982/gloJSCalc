74907226bbeaff7d984f80e7f079889bc3457589
Kirill Lavrov <lkv@myttk.ru>
2021-07-30 16:47:28 +0700

    feat (script.js): Main task is completed

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
-let money, 
-  income, 
-  addExpenses, 
-  deposit, 
-  mission, 
-  period;
+let money = 150000, 
+  income = 'рента', 
+  addExpenses = 'Образование, Автомобиль, Дети, Отдых', 
+  deposit = true, 
+  mission = 500000, 
+  period = 3;
 
-alert('Переменные были созданы.');
-console.log('Хотя, вернее сказать: "Переменные были объявлены"?!');
+console.log('Переменная (money):\n\tтип данных:', 
+  typeof money, '\n\tзначение:', money,
+  '\n\nПеременная (income):\n\tтип данных:', 
+  typeof income, '\n\tзначение:', income,
+  '\n\nПеременная (deposit):\n\tтип данных:', 
+  typeof deposit, '\n\tзначение:', deposit);
+
+console.log('Длина строки (addExpenses):', addExpenses.length);
+
+console.log(`Период равен ${period} месяц${period === 1 ?
+   '' : period !== 0 && period < 5 ? 'а' : 'ев'}`);
+
+console.log(`Цель заработать ${mission} рубл${mission % 10 === 1 ?
+   'ь' : mission % 10 !== 0 && mission % 10 < 5 ?
+    'я' : 'ей'}`);
+
+addExpenses = addExpenses.toLocaleLowerCase();
+ console.log('Список дополнительных рассходов (addExpenses):\n\t', addExpenses.split(', '));
+
+
