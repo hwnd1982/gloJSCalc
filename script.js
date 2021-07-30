bb6ff14aa493cab83e141a9f7c36ccdb58f1ad10
Kirill Lavrov <lkv@myttk.ru>
2021-07-30 22:23:20 +0700

    fix (script.js): Used template strings

--- script.js
+++ script.js
@@ -3,26 +3,29 @@ const money = 150000,
   addExpenses = 'Образование, Автомобиль, Дети, Отдых', 
   deposit = true, 
   mission = 500021, 
-  period = 21;
-let budgetDay = money / 30;
+  period = 21,
+  budgetDay = money / 30;
 
-console.log('Переменная (money):\n\tтип данных:', 
-  typeof money, '\n\tзначение:', money,
-  '\n\nПеременная (income):\n\tтип данных:', 
-  typeof income, '\n\tзначение:', income,
-  '\n\nПеременная (deposit):\n\tтип данных:', 
-  typeof deposit, '\n\tзначение:', deposit);
+console.log(`Переменная (money):
+  тип данных: ${typeof money};
+  значение: ${money};
+Переменная (income):
+  тип данных: ${typeof income};
+  значение: ${income};
+Переменная (deposit):
+  тип данных: ${typeof deposit};
+  значение: ${deposit};`);
 
-console.log('Длина строки (addExpenses):', addExpenses.length);
+console.log(`Длина строки (addExpenses): ${addExpenses.length}`);
 
 console.log(`Период равен ${period} месяц${period % 10 === 1 ?
-   '' : period % 10 !== 0 && period % 10 < 5 ? 'а' : 'ев'}`);
+  '' : period % 10 !== 0 && period % 10 < 5 ? 'а' : 'ев'}`);
 
 console.log(`Цель заработать ${mission} рубл${mission % 10 === 1 ?
-   'ь' : mission % 10 !== 0 && mission % 10 < 5 ?
-    'я' : 'ей'}`);
+  'ь' : mission % 10 !== 0 && mission % 10 < 5 ?
+  'я' : 'ей'}`);
 
- console.log('Список дополнительных рассходов (addExpenses):\n\t', 
-  addExpenses.toLocaleLowerCase().split(', '));
+console.log(`Список дополнительных рассходов (addExpenses): 
+  ${addExpenses.toLocaleLowerCase().split(',')}.`);
   
-console.log('Дневной бюджет: ', budgetDay);
+console.log(`Дневной бюджет: ${budgetDay}`);
