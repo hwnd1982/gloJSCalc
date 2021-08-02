4aec92f478dccf9f2ef44aa80e304cc6fdf34fc2
Kirill Lavrov <lkv@myttk.ru>
2021-08-02 14:43:07 +0700

    feat (script.js): Main task is completed

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
-const money = 150000, 
+'use strict';
+
+const money = +prompt('Ваш месячный доход?'), 
   income = 'рента', 
-  addExpenses = 'Образование, Автомобиль, Дети, Отдых', 
-  deposit = true, 
+  addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую:') || '',
+  deposit = confirm('Есть ли у вас депозит в банке?'), 
   mission = 500021, 
-  period = 21,
-  budgetDay = money / 30;
+  expenses1 = prompt('Введите обязательную статью расходов:'),
+  amount1 = +prompt('Во сколько это обойдется?'),
+  expenses2 = prompt('Введите обязательную статью расходов:'),
+  amount2 = +prompt('Во сколько это обойдется?'),
+  budgetMonth = money - amount1 - amount2,
+  budgetDay = Math.floor(budgetMonth / 30, 0),
+  period = Math.ceil(mission / budgetMonth, 0);
 
 console.log(`Переменная (money):
   тип данных: ${typeof money};
-  значение: ${money};
+  значение: ${money || 'Данные введены некорректно!'};
 Переменная (income):
   тип данных: ${typeof income};
   значение: ${income};
@@ -18,14 +25,31 @@ console.log(`Переменная (money):
 
 console.log(`Длина строки (addExpenses): ${addExpenses.length}`);
 
-console.log(`Период равен ${period} месяц${period % 10 === 1 ?
-  '' : period % 10 !== 0 && period % 10 < 5 ? 'а' : 'ев'}`);
-
 console.log(`Цель заработать ${mission} рубл${mission % 10 === 1 ?
   'ь' : mission % 10 !== 0 && mission % 10 < 5 ?
   'я' : 'ей'}`);
 
+console.log(`Удастся накопить за: ${period} месяц${period % 10 === 1 ?
+  '' : period % 10 !== 0 && period % 10 < 5 ? 'а' : 'ев'}`);
+
 console.log(`Список дополнительных рассходов (addExpenses): 
-  ${addExpenses.toLocaleLowerCase().split(',')}.`);
-  
+  ${addExpenses.toLocaleLowerCase().split(',').length === 1 && 
+  !addExpenses.toLocaleLowerCase().split(',')[0] ? 
+  'список пуст...' : 
+  addExpenses.toLocaleLowerCase().split(',')}`);
+console.log(`Бюджет на месяц: ${budgetMonth}`);
 console.log(`Дневной бюджет: ${budgetDay}`);
+
+if (budgetDay > 1200) {
+  console.log('У вас высокий уровень дохода');
+} else {
+  if (budgetDay <= 1200 && budgetDay >= 600) {
+    console.log('У вас средний уровень дохода');
+  } else {
+    if (budgetDay < 600 &&  budgetDay >= 0){
+      console.log('К сожалению у вас уровень дохода ниже среднего');
+    } else {
+      console.log('Что то пошло не так');
+    }
+  }
+}
